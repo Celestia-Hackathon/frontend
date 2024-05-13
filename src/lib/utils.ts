39c0e38326e1068eda3ddaa81a0c2f07a905fe4b
Daniel Yuki <danielyukihiga1@gmail.com>
2024-05-12 22:40:35 -0300

    quick tests

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,18 +8,18 @@ export function cn(...inputs: ClassValue[]) {
 export function getColor(color: string) {
   switch(color) {
     case "Common":
-      return {text: "text-common", border: "border-common", bg: "bg-common"};
+      return {text: "text-common", border: "border-common", bg: "bg-common", shadow: "shadow-common"};
     case "Uncommon":
-      return {text: "text-uncommon", border: "border-uncommon", bg: "bg-uncommon"};
+      return {text: "text-uncommon", border: "border-uncommon", bg: "bg-uncommon", shadow: "shadow-uncommon"};
     case "Rare":
-      return {text: "text-rare", border: "border-rare", bg: "bg-rare"};
+      return {text: "text-rare", border: "border-rare", bg: "bg-rare", shadow: "shadow-rare"};
     case "Epic":
-      return {text: "text-epic", border: "border-epic", bg: "bg-epic"};
+      return {text: "text-epic", border: "border-epic", bg: "bg-epic", shadow: "shadow-epic"};
     case "Legendary":
-      return {text: "text-legendary", border: "border-legendary", bg: "bg-legendary"};
+      return {text: "text-legendary", border: "border-legendary", bg: "bg-legendary", shadow: "shadow-legendary"};
     case "Special":
-      return {text: "text-special", border: "border-special", bg: "bg-special"};
+      return {text: "text-special", border: "border-special", bg: "bg-special", shadow: "shadow-special"};
     default:
-      return {text: "text-black", border: "border-epic", bg: "bg-epic"};
+      return {text: "text-black", border: "border-epic", bg: "bg-epic", shadow: "shadow-epic"};
   }
 }
